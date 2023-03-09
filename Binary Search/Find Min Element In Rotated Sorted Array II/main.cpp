0fbf21adeb2e012fe33f8019f04b049544c94381
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2023-03-09 20:17:36 +0530

    Create main.cpp

---
+++ Binary Search/Find Min Element In Rotated Sorted Array II/main.cpp
@@ -0,0 +1,26 @@
+/*
+Logic: 
+Here due to duplicates we have to check this condition : nums[low]==nums[mid]==nums[high], round down the high by 1
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low=0,n=nums.size();
+        int high=nums.size()-1;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]>nums[high]){  // condition for right unsorted half
+                low=mid+1;
+            }else if(nums[mid]<nums[high]){  // condition for left unsorted half ( here nums[mid]>nums[low] can also work )
+                high=mid;
+            }else{
+                high--;   // condition : nums[low]==nums[mid]==nums[high]
+            }
+        }
+        return nums[low];
+                         
+    }
+};
