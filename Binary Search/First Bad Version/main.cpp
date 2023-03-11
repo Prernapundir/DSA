e29eb49f0d93c2543d061754a643cff40952b13a
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2023-03-11 19:47:45 +0530

    Create main.cpp

---
+++ Binary Search/First Bad Version/main.cpp
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/first-bad-version/description/
+
+
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=1,high=n;
+
+        while(low<high){
+            int mid=low+(high-low)/2;
+
+            if(isBadVersion(mid)){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
