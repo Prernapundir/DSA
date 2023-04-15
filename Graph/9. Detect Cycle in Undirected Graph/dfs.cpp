de1bc9207950edcc1523a6327efc02a60508d0a0
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2023-04-15 19:00:23 +0530

    Update dfs.cpp

--- Graph/9. Detect Cycle in Undirected Graph/dfs.cpp
+++ Graph/9. Detect Cycle in Undirected Graph/dfs.cpp
@@ -1,15 +1,14 @@
 class Solution {
   public:
   
-    // dffs code
-    bool dfs(vector<int>&parent,vector<bool>&vis,int src,vector<int>adj[]){
+   //dfs
+    bool dfs(int parent,vector<bool>&vis,int src,vector<int>adj[]){
         vis[src]=true;
         
         for(auto x: adj[src]){
             if(!vis[x]){
-                parent[x]=src;
-                if(dfs(parent,vis,x,adj)) return true;
-            }else if(parent[src]!=x){   // condition to check for cycle { important to remember }
+                if(dfs(src,vis,x,adj)) return true;
+            }else if(parent!=x){
                 return true;
             }
         }
@@ -18,13 +17,11 @@ class Solution {
     
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
-        
-        vector<int> parent(V,-1);
         vector<bool> vis(V);
         
         for(int i=0;i<V;i++){    // loop for disconnected components
             if(!vis[i]){
-                if(dfs(parent,vis,i,adj)) return true;
+                if(dfs(-1,vis,i,adj)) return true;
             }
         }
         
